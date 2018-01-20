206c1ef6ea7c4411659978caa35801a87290ed14
Cristian Roldán <roldanchanad@gmail.com>
2018-01-20 09:48:33 -0600

    Prueba 1

---
+++ Test/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese su nombre:");
+            string cadena = Console.ReadLine();
+            Console.WriteLine("Mi nombre es: " + cadena);
+            Console.ReadKey();
+        }
+    }
+}
